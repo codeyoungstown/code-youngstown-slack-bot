f2e64af87a0ee64b7c6bdb14e3de32414d02e7c0
JoeDuncko <joe@drund.com>
2018-12-08 16:17:59 -0500

    Expand intro text

--- skills/sample_events.js
+++ skills/sample_events.js
@@ -2,7 +2,7 @@ module.exports = function(controller) {
 
     controller.on('user_channel_join,user_group_join', function(bot, message) {
 
-        bot.reply(message, 'Welcome, <@' + message.user + '>');
+        bot.reply(message, 'Welcome to Code Youngstown, <@' + message.user + '>! Post an intro so everyone knows who you are and what you\'re working on.');
 
     });
 
